3ad6903df8c56fbadc70d25cafdb03af3c6e91f4
Shahar Frank <srfrnk@gmail.com>
2021-09-30 08:14:39 +0100

    updated image versions

--- initializer/initializer.sh
+++ initializer/initializer.sh
@@ -5,8 +5,15 @@ status_code=0
 echo "Waiting for index to be created..."
 
 while [[ "${status_code}" != "200" ]]; do
-  status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -X POST -H "Content-Type: application/json" -H "kbn-xsrf: anything" "http://efk-kibana:5601/api/saved_objects/index-pattern/logstash-*" \
-    -d '{"attributes":{"title":"logstash-*","timeFieldName":"@timestamp"}}')
+  status_code=$(curl -o /dev/null -s -w "%{http_code}\n" -X POST "http://efk-kibana:5601/api/index_patterns/index_pattern" -H 'kbn-xsrf: true' -H 'Content-Type: application/json' -d'
+  {
+    "override": true,
+    "refresh_fields": true,
+    "index_pattern": {
+      "title": "logstash-*",
+      "timeFieldName":"@timestamp"
+    }
+  }')
   echo "."
   sleep 10
 done
